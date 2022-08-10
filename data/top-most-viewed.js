178b8e9fd0c274ed5657df2913af64d2b94f1497
Ali810365 <mabdi@sea.yearup.org>
2022-08-09 23:04:13 -0700

    updated

--- data/top-most-viewed.js
+++ data/top-most-viewed.js
@@ -5,8 +5,8 @@
       "Title": "On & On",
       "Artist": "Cartoon ",
       "Featuring": "Daniel Levi",
-      "Release Year": 2015,
-      "views": 465183688,
+      "Year": 2015,
+      "Views": 465183688,
       "Play": "on-and-on.mp3",
       "Image": "cartoon.jpg"
     },
@@ -14,8 +14,8 @@
       "Title": "Heroes Tonight",
       "Artist": "Janji ",
       "Featuring": "Johnning",
-      "Release Year": 2015,
-      "views": 290433425,
+      "Year": 2015,
+      "Views": 290433425,
       "Play": "heroes-tonight.mp3",
       "Image": "janji.jpg"
     },
@@ -23,8 +23,8 @@
       "Title": "Invincible ",
       "Artist": "Deaf Kev",
       "Featuring": "NCS Release",
-      "Release Year": 2015,
-      "views": 262727215,
+      "Year": 2015,
+      "Views": 262727215,
       "Play": "invincible.mp3 ",
       "Image": "kev.jpg"
     },
@@ -32,8 +32,8 @@
       "Title": "My Heart",
       "Artist": "Different Heaven",
       "Featuring": "EH!DE",
-      "Release Year": 2013,
-      "views": 252113276,
+      "Year": 2013,
+      "Views": 252113276,
       "Play": "my-heart.mp3",
       "Image": "heaven.jpg"
     },
@@ -41,8 +41,8 @@
       "Title": "Blank ",
       "Artist": "Disfigure ",
       "Featuring": "NCS Release",
-      "Release Year": 2013,
-      "views": 227611960,
+      "Year": 2013,
+      "Views": 227611960,
       "Play": "blank.mp3",
       "Image": "disfigure.jpg"
     }
