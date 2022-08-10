19da8445cd219f911eb2416908c930fe427fcae3
Ali810365 <mabdi@sea.yearup.org>
2022-08-09 23:58:03 -0700

    updated

---
+++ data/my-top-favorites.js
@@ -0,0 +1,95 @@
+{
+  "title": "My Top 10 Favorites Of The Top 10 Most Viewed",
+  "song": [
+    {
+      "Title": "Invincible ",
+      "Artist": "Deaf Kev",
+      "Featuring": "NCS Release",
+      "Year": 2015,
+      "Views": 262727215,
+      "Play": "invincible.mp3 ",
+      "Image": "kev.jpg"
+    },
+    {
+      "Title": "Mortals ",
+      "Artist": "Warriyo ",
+      "Featuring": "Laura Brehm",
+      "Year": 2016,
+      "Views": 224800388,
+      "Play": "mortals.mp3",
+      "Image": "warriyo.jpg"
+    },
+    {
+      "Title": "Heroes Tonight",
+      "Artist": "Janji ",
+      "Featuring": "Johnning",
+      "Year": 2015,
+      "Views": 290433425,
+      "Play": "heroes-tonight.mp3",
+      "Image": "janji.jpg"
+    },
+    {
+      "Title": "On & On",
+      "Artist": "Cartoon ",
+      "Featuring": "Daniel Levi",
+      "Year": 2015,
+      "Views": 465183688,
+      "Play": "on-and-on.mp3",
+      "Image": "cartoon.jpg"
+    },
+    {
+      "Title": "Fearless ",
+      "Artist": "Lost Sky",
+      "Featuring": "Chris Linton",
+      "Year": 2017,
+      "Views": 169051094,
+      "Play": "fearless.mp3",
+      "Image": "sky.jpg"
+    },
+    {
+      "Title": "Why We Lose ",
+      "Artist": "Cartoon ",
+      "Featuring": "Coleman Trapp",
+      "Year": 2015,
+      "Views": 195765339,
+      "Play": "why-we-lose.mp3",
+      "Image": "cartoon.jpg"
+    },
+    {
+      "Title": "Symbolism ",
+      "Artist": "Electro Light",
+      "Featuring": "NCS Release",
+      "Year": 2014,
+      "Views": 203581777,
+      "Play": "symbolism.mp3",
+      "Image": "electrp.jpg"
+    },
+    {
+      "Title": "Sky High",
+      "Artist": "Elektronomia ",
+      "Featuring": "NCS Release",
+      "Year": 2017,
+      "Views": 221266884,
+      "Play": "sky-high.mp3",
+      "Image": "elektronomia.jpg"
+    },
+    {
+      "Title": "Blank ",
+      "Artist": "Disfigure ",
+      "Featuring": "NCS Release",
+      "Year": 2013,
+      "Views": 227611960,
+      "Play": "blank.mp3",
+      "Image": "disfigure.jpg"
+    },
+    {
+      "Title": "My Heart",
+      "Artist": "Different Heaven",
+      "Featuring": "EH!DE",
+      "Year": 2013,
+      "Views": 252113276,
+      "Play": "my-heart.mp3",
+      "Image": "heaven.jpg"
+    }
+   ]
+}
