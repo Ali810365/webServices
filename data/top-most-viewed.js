0be7f55f80bb694f493facb218d936eae76f42bf
Ali810365 <mabdi@sea.yearup.org>
2022-08-09 23:45:09 -0700

    updated

--- data/top-most-viewed.js
+++ data/top-most-viewed.js
@@ -45,6 +45,51 @@
       "Views": 227611960,
       "Play": "blank.mp3",
       "Image": "disfigure.jpg"
+    },
+    {
+      "Title": "Mortals ",
+      "Artist": "Warriyo ",
+      "Featuring": "Laura Brehm",
+      "Year": 2016,
+      "Views": 224800388,
+      "Play": "mortals.mp3",
+      "Image": "warriyo.jpg"
+    },
+    {
+      "Title": "Sky High",
+      "Artist": "Elektronomia ",
+      "Featuring": "NCS Release",
+      "Year": 2017,
+      "Views": 221266884,
+      "Play": "sky-high.mp3",
+      "Image": "elektronomia.jpg"
+    },
+    {
+      "Title": "Symbolism ",
+      "Artist": "Electro Light",
+      "Featuring": "NCS Release",
+      "Year": 2014,
+      "Views": 203581777,
+      "Play": "symbolism.mp3",
+      "Image": "electrp.jpg"
+    },
+    {
+      "Title": "Cartoon ",
+      "Artist": "Cartoon ",
+      "Featuring": "Coleman Trapp",
+      "Year": 2015,
+      "Views": 195765339,
+      "Play": "why-we-lose.mp3",
+      "Image": "cartoon.jpg"
+    },
+    {
+      "Title": "Fearless ",
+      "Artist": "Lost Sky",
+      "Featuring": "Chris Linton",
+      "Year": 2017,
+      "Views": 169051094,
+      "Play": "fearless.mp3",
+      "Image": "sky.jpg"
     }
    ]
 }
