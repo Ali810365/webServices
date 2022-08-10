989dbcc41293ce76c5206373487e206ed9ba119f
Ali810365 <mabdi@sea.yearup.org>
2022-08-09 22:48:38 -0700

    updated

--- data/top-most-viewed.js
+++ data/top-most-viewed.js
@@ -5,7 +5,7 @@
       "Title": "On & On",
       "Artist": "Cartoon ",
       "Featuring": "Daniel Levi",
-      "Year": 2015,
+      "Release Year": 2015,
       "views": 465183688,
       "Play": "on-and-on.mp3",
       "Image": "cartoon.jpg"
@@ -14,10 +14,37 @@
       "Title": "Heroes Tonight",
       "Artist": "Janji ",
       "Featuring": "Johnning",
-      "Year": 2015,
+      "Release Year": 2015,
       "views": 290433425,
       "Play": "heroes-tonight.mp3",
       "Image": "janji.jpg"
+    },
+    {
+      "Title": "Invincible ",
+      "Artist": "Deaf Kev",
+      "Featuring": "NCS Release",
+      "Release Year": 2015,
+      "views": 262727215,
+      "Play": "invincible.mp3 ",
+      "Image": "kev.jpg"
+    },
+    {
+      "Title": "My Heart",
+      "Artist": "Different Heaven",
+      "Featuring": "EH!DE",
+      "Release Year": 2013,
+      "views": 252113276,
+      "Play": "my-heart.mp3",
+      "Image": "heaven.jpg"
+    },
+    {
+      "Title": "Blank ",
+      "Artist": "Disfigure ",
+      "Featuring": "NCS Release",
+      "Release Year": 2013,
+      "views": 227611960,
+      "Play": "blank.mp3",
+      "Image": "disfigure.jpg"
     }
-  ]
+   ]
 }
